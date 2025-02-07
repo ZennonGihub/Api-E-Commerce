28362229bd921aa25daff515f5eb6000d4b696aa
Zennon <zeitaa34@gmail.com>
2025-02-07 19:10:45 -0300

    Creaccion de documento docker

--- api/routes/index.js
+++ api/routes/index.js
@@ -7,9 +7,9 @@ const usuarioRouter = require('./usuarios');
 function routerApi(app) {
   const router = express.Router();
   app.use('/api/v1', router)
-  router.use('/Articulos', productsRouter)
-  router.use('/SanLuis', informacionRouter)
-  router.use('/Usuarios', usuarioRouter)
+  router.use('/articulos', productsRouter)
+  router.use('/sanLuis', informacionRouter)
+  router.use('/usuarios', usuarioRouter)
 }
 
 module.exports = routerApi
