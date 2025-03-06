73c3374612077e285c34cf355cbe92232a4c5ea0
Zennon <zeitaa34@gmail.com>
2025-03-06 14:23:56 -0300

    bug resuelto del orm

--- api/db/models/user.model.js
+++ api/db/models/user.model.js
@@ -1,4 +1,5 @@
-const { Model, DataTypes, Sequelize } = require('sequelize')
+const { Model, DataTypes, Sequelize } = require('sequelize');
+const  sequelize  = require('../../libs/sequelize');
 
 const USER_TABLE = 'users';
 
--- api/libs/sequelize.js
+++ api/libs/sequelize.js
@@ -1,22 +1,20 @@
-const { Sequelize } = require('sequelize');
+const  Sequelize  = require('sequelize');
 const { config } = require('../config/config')
 const setupModels = require('../db/models/index')
 
 console.log('CONFIG:', config);
 
-
 const USER = encodeURIComponent(config.DbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
 const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
-console.log('Database URI:', URI);
 
-const sequelize = new Sequelize(URI, {
+const sequelize = new Sequelize (URI, {
   dialect: 'postgres',
-  logging: true,
+  logging: false,
 });
 
 setupModels(sequelize)
 
 sequelize.sync();
 
-module.exports = { sequelize };
+module.exports = sequelize ;
