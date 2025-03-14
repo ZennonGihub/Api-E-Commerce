58289a85d0c5a278b64dcd18787b73fbbecc24d5
Zennon <zeitaa34@gmail.com>
2025-03-14 17:47:34 -0300

    rutas de carrito de compras

--- api/routes/carritoDeCompras.router.js
+++ api/routes/carritoDeCompras.router.js
@@ -1,9 +1,53 @@
 const express = require('express')
+const validatorHandler = require('../middlewares/validator.handler')
+const {updateItemSchema, addItemSchema}= require('./../schemas/carroDeCompras.schema');
+const CartUsers = require('./../services/carroDeCompras.services');
+const boom = require('@hapi/boom')
 
 const router = express.Router();
+const service = new CartUsers();
 
-router.get('/Conocenos', (req,res) => {
-
+router.get('/:userId', async (req, res, next) => {
+  try {
+    const { userId } = req.params;
+    const cart = await service.getCart(userId);
+    res.json(cart)
+  } catch (err) {
+    next(err)
+  }
 })
+
+router.post('/agregar',
+  validatorHandler(addItemSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const newItem = await service.addItem(req.body);
+      res.status(201).json(newItem);
+    } catch (error) {
+      next(error);
+    }
+})
+
+router.put('/cambiarCarro/:cartItemId', validatorHandler(updateItemSchema, 'body'),
+async (req, res, next) =>   async (req, res, next) => {
+  try {
+    const { cartItemId } = req.params;
+    const updatedItem = await service.updateItem(cartItemId, req.body.cantidad);
+    res.json(updatedItem);
+  } catch (error) {
+    next(error);
+  }
+}
+);
+
+router.delete('/remove/:cartItemId', async (req, res, next) => {
+  try {
+    const { cartItemId } = req.params;
+    const result = await service.removeItem(cartItemId);
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
--- api/schemas/carroDeCompras.schema.js
+++ api/schemas/carroDeCompras.schema.js
@@ -1,12 +1,12 @@
 const joi = require('joi')
 
-const cartId = joi.string().integer();
+const cartId = joi.string();
 const userId = joi.string().alphanum().min(3).max(10).required();
-const amount = joi.string().integer().min(1).required();
+const amount = joi.number().integer().min(1).required();
 const productId = joi.number().integer().required();
 const cartItemId = joi.number().integer().required();
 
-const addItemCarroSchema = joi.object({
+const addItemSchema = joi.object({
   userId: userId.required(),
   productId: productId.required(),
   amount: amount.required()
@@ -18,11 +18,11 @@ const addItemCarroSchema = joi.object({
 //  amount: amount.required()
 //})
 
-const updateItemSchema = Joi.object({
+const updateItemSchema = joi.object({
   cartItemId: cartItemId.required(),
-  cantidad: cantidad.required(),
+  amount: amount.required(),
   productId: productId.required()
 });
 
 
-module.exports = {updateItemSchema, addItemCarroSchema}
+module.exports = {updateItemSchema, addItemSchema}
